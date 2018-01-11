b42e1fed98dff418ded7b8340ffdd881f575a90c
jws8787 <jwsxzx@naver.com>
2018-01-12 00:08:47 +0900

    study

---
+++ Using_simple_Arrangement.rs
@@ -0,0 +1,15 @@
+fn main()
+
+{
+
+    let animal = ["cat","dog"];
+    
+    for ix in 0..animal.len()
+    
+    {
+        
+         println!("animal no {} is {}",ix,animal[ix]);
+    
+    }
+
+}
---
+++ simple_Vector.rs
@@ -0,0 +1,15 @@
+fn main()
+
+{
+    
+    let value = vec! [1,5,9];
+    
+    for n in value
+    
+    {
+        
+         println!("{}",n);
+    
+    }
+
+}
